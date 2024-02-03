80fea1a75b33d9cccde017940bc29f3983f02ae5
zzex3on <zzeonii0612@gmail.com>
2024-02-03 23:13:38 +0900

    필드와 컬럼 매핑 강의 관련 어노테이션 추가

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -8,33 +8,17 @@ import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
-        // 애플리케이션 로딩 시점에 딱 하나만 만들어둬야 함,
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-        // DB Connection을 하고 close 하는 일련의 과정에서 EntityManager를 꼭 만들어줘야 함
-        // DB당 하나 생성
         EntityManager em = emf.createEntityManager();
-        // 기능 하나에 쓰고 버림 -> 스레드간에 공유하지 않음! 일회용성
 
-        // JPA에서는 트랜잭션이라는 단위가 매우 중요함! 데이터를 변경하는 모든 작업은 반드시 트랜잭션 내에서 수행되어야 함
-        // DB Connection 하나 받는다는 느낌
         EntityTransaction tx = em.getTransaction(); // 트랜잭션 얻음
         tx.begin(); // 트랜잭션 시작
 
         try {
-            // 비영속
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
+            Member member = em.find(Member.class, 101L);
+            em.clear();
 
-            // 영속
-            System.out.println(":: before ::");
-            em.persist(member);
-            System.out.println(":: after ::");
-
-            Member findMember = em.find(Member.class, 101L);
-
-            System.out.println("findMember.id = " + findMember.getId());
-            System.out.println("findMember.name = " + findMember.getName());
+            Member member2 = em.find(Member.class, 150L);
 
             tx.commit();
         } catch(Exception e) {
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,41 +1,32 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
-// jpa를 사용하는 애구나~ 라고 인식, 관리해야함을 인지 (어노테이션 필수!)
 @Entity
-// USER 라는 테이블에 동작하도록 표기
-//@Table(name = "USER")
 public class Member {
-    @Id // PK 지정
+
+    @Id
     private Long id;
-    //@Column(name = "username") // DB의 컬럼명을 명시
-    private String name;
+
+    @Column(name = "name")
+    private String username;
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
 
     public Member() {
     }
 
-    public Member(Long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
